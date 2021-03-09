a21d6b8a29f1914296623f932eb61306b9c27fc4
Sergey <beketov.sergey1993@gmail.com>
2021-03-09 20:30:24 +0200

    Initial

---
+++ HomeTasks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
